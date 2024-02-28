d95a0bb82923bf6a510c78bd19af9ea614cdbf48
Edet Effiong <greatstory247@gmail.com>
2024-02-28 17:41:57 +0100

    cart page created

--- my-FOOD-mobile/scr/pages/home/Cart.jsx
+++ my-FOOD-mobile/scr/pages/home/Cart.jsx
@@ -1,167 +1,130 @@
-import *  as React from 'react';
-import { useState } from 'react';
-import { ScrollView, Text, View , FlatList, StyleSheet, Image, Pressable, Animated, TouchableOpacity} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SearchBar } from '@rneui/themed';
+/* This file has been downloaded from rnexamples.com */
+/* If You want to help us please go here https://www.rnexamples.com/help-us */
+import React, { useState } from 'react'
+import { View, Text, Button, Image, StyleSheet } from 'react-native'
 
+const items = [
+  { id: 1, name: 'Item 1', price: '$10', quantity: 1, image: 'https://www.bootdey.com/image/280x280/00FFFF/000000' },
+  { id: 2, name: 'Item 2', price: '$20', quantity: 1, image: 'https://www.bootdey.com/image/280x280/FF00FF/000000' },
+  { id: 3, name: 'Item 3', price: '$30', quantity: 1, image: 'https://www.bootdey.com/image/280x280/FF7F50/000000' },
+]
 
-import Cart from './Cart';
-import Notification from './Notification';
-import { SafeAreaView } from 'react-native-safe-area-context';
+const ShoppingCart = () => {
+  const [cartItems, setCartItems] = useState(items)
 
-
-const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
-
-
-
-// const Tab = createBottomTabNavigator();
-const Home: React.FunctionComponent<SearchBarComponentProps> = () => {
-  const [search, setSearch] = useState("");
-  const [seeAllCategory, setSeeAllCategory] = useState(false);
-
-
-  const prevImage1 = 'https://cdn.hashnode.com/res/hashnode/image/upload/v1705174513487/2429e79d-0446-47d8-8f68-453ce76daa40.png'
-
-
-  const updateSearch = (search) => {
-    setSearch(search);
-  };
-
-  // user greeting
-  const getGreeting =()=> {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-  
-    let greeting;
-    if (currentHour >= 5 && currentHour < 12) {
-      greeting = 'Good morning';
-    } else if (currentHour >= 12 && currentHour < 18) {
-      greeting = 'Good afternoon';
-    } else if (currentHour >= 18 && currentHour < 22) {
-      greeting = 'Good evening';
-    } else {
-      greeting = 'Good night';
-    }
-    return greeting;
+  const addItem = item => {
+    setCartItems([...cartItems, item])
   }
 
-  // all categories date
-  const _allCategoriesData =[
-    {
-      id: '1',
-      name: 'Pizza',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    },
-    {
-      id: '2',
-      name: 'Burger',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    },
-    {
-      id: '3',
-      name: 'Shawarmer',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    },
-    {
-      id: '4',
-      name: 'Rice',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    },
-    {
-      id: '5',
-      name: 'Snacks',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    },
-    {
-      id: '6',
-      name: 'Wine',
-      image: <Image source={{uri: prevImage1}} style={styles.prevImage} />,
-    }
-  ]
-  // items to render or all categories flatlist
-  const renderItem = ({ item }) => (
-    <Pressable
-      style={[
-        styles.scrollImgContainer,
-        // selectedEmployee.some((f) => f.id === item.id) && styles.selectedItem,
-      ]}
-      // onPress={() => togglePreview(item)}
-    >
-      <Text>{item.icon}</Text>
-      <Text style={styles.scrollImgText}>{item.name}</Text>
-    </Pressable>
-  );
-  
-  const toggleSeeAllCategory = () => {
-    setSeeAllCategory(!seeAllCategory);
-  };
+  const removeItem = item => {
+    setCartItems(cartItems.filter(i => i !== item))
+  }
+
+  const increaseQuantity = item => {
+    const newCartItems = cartItems.map(i => {
+      if (i === item) {
+        return { ...i, quantity: i.quantity + 1 }
+      }
+      return i
+    })
+    setCartItems(newCartItems)
+  }
+
+  const decreaseQuantity = item => {
+    const newCartItems = cartItems.map(i => {
+      if (i === item && i.quantity > 1) {
+        return { ...i, quantity: i.quantity - 1 }
+      }
+      return i
+    })
+    setCartItems(newCartItems)
+  }
 
   return (
-    <SafeAreaView>
-    <View>
-      <Text>Hey Edet,</Text><Text>{getGreeting()}</Text>
-      <SearchBar
-        ref={search => this.search = search}
-        placeholder="Type Here..."
-        onChangeText={updateSearch}
-        value={search}
+    <View style={styles.container}>
+      <Text style={styles.tittle}>Shopping Cart</Text>
+      {cartItems.map(item => (
+        <View key={item.id} style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+          <View style={styles.infoContainer}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.price}>{item.price}</Text>
+          </View>
+          <View style={styles.quantityContainer}>
+            <Button title="-" onPress={() => decreaseQuantity(item)} />
+            <Text style={styles.quantity}>{item.quantity}</Text>
+            <Button title="+" onPress={() => increaseQuantity(item)} />
+          </View>
+          <View style={styles.removeButton}>
+            <Button title="Remove" onPress={() => removeItem(item)} />
+          </View>
+        </View>
+      ))}
+      <Button
+        title="Add Item"
+        onPress={() =>
+          addItem({
+            id: Math.random(),
+            name: 'Item 1',
+            price: '$10',
+            image: 'https://www.bootdey.com/image/280x280/00FFFF/000000',
+            quantity: 1,
+          })
+        }
       />
     </View>
-    <ScrollView>
-    {/* all categories */}
-      <View>
-        <View style={styles.scrollHeaderContainer}>
-          <Text style={styles.scrollHeaderText1}>
-            All Categories
-          </Text>
-          <TouchableOpacity onPress={toggleSeeAllCategory}>
-          <Text
-          style={styles.scrollHeaderText2}
-          >See All</Text>
-          </TouchableOpacity>
-        </View>
-        <AnimatedFlatList
-          showsHorizontalScrollIndicator={false}
-          horizontal={!seeAllCategory}
-          data={_allCategoriesData}
-          // renderItem={({ item }) => <Item item={item} />}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          // style={[styles.flatList, { height: seeAllCategory? 'auto' : 50}]}
-        />
-      </View>
-
-
-    </ScrollView>
-
-    </SafeAreaView>
-  );
+  )
 }
-export default Home
-
-
 
 const styles = StyleSheet.create({
-  body:{
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal:10,
   },
-  view1:{
-
+  tittle:{
+    fontSize:24,
+    marginBottom:20,
   },
-  scrollImgContainer: {
-    // flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginVertical: 7,
-    marginHorizontal: 3,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    borderColor: "#ddd",
-    borderRadius: 8,
-    borderWidth: 1,
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    shadowColor: '#cccccc',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    backgroundColor: '#fff',
+    marginBottom: 10,
   },
-  selectedItem: {
-    backgroundColor: "#add8e6", // Light blue background for selected items
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  infoContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+  },
+  price: {
+    fontSize: 16,
+    color: '#888',
+  },
+  removeButton: {
+    alignItems: 'center',
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantity: {
+    marginRight: 10,
   },
 })
+
+export default ShoppingCart
