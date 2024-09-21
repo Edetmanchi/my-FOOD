c5661dbf14719e9213a66eeb40490ed9c54f34d1
Edet Effiong <greatstory247@gmail.com>
2024-09-22 00:00:44 +0100

    Get category AOI added

--- my-FOOD-website/src/app/_utils/GlobalApi.js
+++ my-FOOD-website/src/app/_utils/GlobalApi.js
@@ -1,7 +1,25 @@
-import {gql} =require=("graphql-request")
+// import {gql} =require=("graphql-request")
 
-const MASTER_URL=process.env.NEXT_PUBLIC_BACKEND_API_URL
+const {gql} = require("graphql-require")
 
-const GetCategory = ()=>{
-    const query = 
+const MASTER_URL=process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
+const GetCategory = async()=>{
+    const query = gql`
+    query MyQuery {
+        categories(first: 30) {
+          id
+          name
+          slug
+          description
+          icon {
+            url
+          }
+        }
+      }
+    `
+    const result = await request(MASTER_URL, query)
+    return result
 }
+
+export default GetCategory
